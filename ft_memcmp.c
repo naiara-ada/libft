9d4d58ae986bb1c47cc48cd826be09f5041b8338
naiara-ada <148208715+naiara-ada@users.noreply.github.com>
2024-09-16 16:11:10 +0200

    Add files via upload

---
+++ ft_memcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: narrospi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 11:42:30 by narrospi          #+#    #+#             */
+/*   Updated: 2024/09/12 12:02:06 by narrospi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
+	while (i < n && str1[i] == str2[i])
+		i++;
+	return ((unsigned char) str1[i] - (unsigned char) str2[i]);
+}
