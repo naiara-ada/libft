735cc025967d363c73955430dac09baf98ec7fb1
naiara-ada <148208715+naiara-ada@users.noreply.github.com>
2024-09-19 19:52:04 +0200

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -15,12 +15,12 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t			i;
-	unsigned char	*str1;
-	unsigned char	*str2;
+	const unsigned char	*str1;
+	const unsigned char	*str2;
 
 	i = 0;
-	str1 = (unsigned char *) s1;
-	str2 = (unsigned char *) s2;
+	str1 = (const unsigned char *) s1;
+	str2 = (const unsigned char *) s2;
 	while (i < n)
 	{
 		if (str1[i] != str2[i])
