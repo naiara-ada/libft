a1019f9817808a958cee4a3d716dff7786332afb
naiara-ada <148208715+naiara-ada@users.noreply.github.com>
2024-09-23 15:27:03 +0200

    Add files via upload

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: narrospi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 15:14:58 by narrospi          #+#    #+#             */
+/*   Updated: 2024/09/23 15:21:45 by narrospi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *last;
+
+	last = ft_lstlast(*lst);
+	if (!last)
+		*alst = new;
+	last -> next = new;	
+}
