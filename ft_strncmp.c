ed5fffe6f7429b8ee40e6aaaf13191b30e6fad61
naiara-ada <148208715+naiara-ada@users.noreply.github.com>
2024-09-25 19:33:56 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -20,5 +20,7 @@ int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 	while (i < n && s1[i] == s2[i] && s1[i] != '\0'
 		&& s2[i] != '\0')
 		i++;
+	if (i == n)
+		return (0);
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
